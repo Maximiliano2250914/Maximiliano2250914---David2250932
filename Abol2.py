e2d0f57bb18c6c31e0a7a696bc5cadcc2aff8db9
daviduscategui <uscategui.dav07@gmail.com>
2026-04-07 18:28:23 -0500

    Abol2.py

--- Abol2.py
+++ Abol2.py
@@ -22,10 +22,17 @@ class ListaSE:
         while actual is not None:
             print(actual.data)
             actual=actual.siguiente
+            
+    def apunta(self,data):
+        if self.cabeza is None:
+            return None
+        else:
+            act=self.cabeza
+            while act is not None and act.data!=data:
+                act=act.siguiente
+            return act
         
 arbolito=ListaSE()
-nodo1=ListaSE()
 
-nodo1.agregarInicio(5)
-arbolito.agregarInicio(nodo1)
+arbolito.agregarInicio(67)
 arbolito.imprimir()
