5cf5c71f023198767e48dbc42fdc7e352d619995
daviduscategui <uscategui.dav07@gmail.com>
2026-04-07 18:08:55 -0500

    Abol2.py

--- Abol2.py
+++ Abol2.py
@@ -6,3 +6,26 @@ class Nodo:
 class ListaSE:
     def __init__(self):
         self.cabeza=None
+    
+    def agregarInicio(self,data):
+        nuevo=Nodo(data)
+        if self.cabeza is None:
+            self.cabeza=nuevo
+            return
+        else:
+            nuevo.siguiente=self.cabeza
+            self.cabeza=nuevo
+            return
+    
+    def imprimir(self):
+        actual=self.cabeza
+        while actual is not None:
+            print(actual.data)
+            actual=actual.siguiente
+        
+arbolito=ListaSE()
+nodo1=ListaSE()
+
+nodo1.agregarInicio(5)
+arbolito.agregarInicio(nodo1)
+arbolito.imprimir()
