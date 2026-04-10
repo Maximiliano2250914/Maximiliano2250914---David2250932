7f2b4dec4338ffcc624f4f1142d3d8d956000f1e
daviduscategui <uscategui.dav07@gmail.com>
2026-04-10 18:37:13 -0500

    Abol2.py

--- Abol2.py
+++ Abol2.py
@@ -1,38 +1,23 @@
-class Nodo:
-    def __init__(self, data):
-        self.data=data
-        self.siguiente=None
-        
-class ListaSE:
-    def __init__(self):
-        self.cabeza=None
-    
-    def agregarInicio(self,data):
-        nuevo=Nodo(data)
-        if self.cabeza is None:
-            self.cabeza=nuevo
-            return
-        else:
-            nuevo.siguiente=self.cabeza
-            self.cabeza=nuevo
-            return
-    
-    def imprimir(self):
-        actual=self.cabeza
-        while actual is not None:
-            print(actual.data)
-            actual=actual.siguiente
-            
-    def apunta(self,data):
-        if self.cabeza is None:
-            return None
-        else:
-            act=self.cabeza
-            while act is not None and act.data!=data:
-                act=act.siguiente
-            return act
-        
-arbolito=ListaSE()
-
-arbolito.agregarInicio(67)
-arbolito.imprimir()
+from bigtree import Node
+print("Seleccione una opcion")
+cent=0
+while cent!=5:
+  opc=int(input("1. Ingresar, 2. Peso del árbol, 3. Orden, 4. Altura, 5. Salir: "))
+  if opc==1:
+    a=str(input("Digite el nombre del nodo: "))
+    val=int(input("Digite el valor de prioridad: "))
+    root=Node(a,val)
+    cent2=0
+    while cent2!=2:
+      a=str(input("Digite el nombre del nodo: "))
+      val=int(input("Digite el valor de prioridad: "))
+      par=str(input("Digite el padre del nodo: "))
+      root.add_child(Node(a,valor=val,parent=par))
+      cent2=int(input("1. Agregar hijo, 2. Salir: "))
+    root.children
+  elif opc==2:
+    print(root.value)
+  elif opc==5:
+    cent=5
+  else:
+    print("Opcion invalida")
